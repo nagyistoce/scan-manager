d0597707b06eeab5aac32c14230c7a4b59c50795
oren@andalso.net <oren@andalso.net@be4686e7-f763-aa83-a2ea-18e524bc4576>
2012-04-08 02:40:04 +0000

    Branch for the first build with a working windows standalone app and installer. Backends: Dummy, WIA, PS-ReC.

---
+++ branches/build1/gui/main.py
@@ -0,0 +1,65 @@
+from .common import *
+from . import shooting
+from . import settings
+import backend
+from log import *
+
+import shelve
+import sys
+from dialogs import CrashDialog
+
+class App(Application):
+
+	
+	def init(self):
+		self.db = shelve.open(os.path.join(smDataPath(),'scanmanager.settings'))
+		self.images = []
+
+		backend.apis.loadAll()		
+		backend.apis.openAll(db=self.db)
+		errorText = backend.apis.formatAPIErrors()
+		if errorText:
+			sys.stderr.write(errorText)
+		
+		self.setWindowIcon(QtGui.QIcon(':/scanmanager-16.png'))
+		self.SetupWindow.initialiseOptions()
+		self.SetupWindow.show()
+		self.SetupWindow.loadSettings()
+		
+
+	class SetupWindow(settings.SetupWindow):
+		pass
+
+
+	class MainWindow(shooting.MainWindow):
+		pass
+
+	
+	class Timer(BaseWidget,QtCore.QTimer):
+		
+		def init(self):
+			self.setInterval(100)
+			self.start()
+			
+		def ontimeout(self):
+			if not self.app.MainWindow.isVisible():
+				return
+			for camera in self.app.cameras:
+				camera.ontimer()
+
+
+	def cameraPropertiesChanged(self,camera):
+		ndx = self.cameras.index(camera) + 1
+		cc = getattr(self,'Camera%dControls'%ndx)
+		cc.refreshFromCamera()
+		
+
+	@property
+	def cameras(self):
+		if not hasattr(self,'settings'):
+			return ()
+		if self.settings.mode == Mode.V:
+			return (self.settings.cameraL,self.settings.cameraR)
+		else:
+			return (self.settings.cameraC,)
+
---
+++ branches/build1/scanmanager.py
@@ -0,0 +1,40 @@
+from gui.main import App
+import sys
+import traceback
+
+from gui.dialogs import CrashDialog
+
+import backend
+from log import *
+
+import resources
+
+
+def excepthook(excType, excValue, tracebackobj):
+	"""
+	Global catchall for unhandled exceptions
+	"""
+	global app
+	excInfo = (excType, excValue, tracebackobj)
+	text = logException(excInfo=excInfo)
+	try:
+		dialog = CrashDialog(parent=app.activeWindow(),html='<pre>%s</pre>'%text)
+		dialog.open()
+	except:
+		pass
+
+
+if __name__ == '__main__':
+	
+	configureLogging()
+	app = App(sys.argv)
+	sys.excepthook = excepthook
+	app.exec_()
+	for api in backend.apis:
+		try: api.saveSettings()
+		except: pass
+		try: api.close()
+		except: pass
+	try: app.db.close()
+	except: pass
+	sys.exit()
