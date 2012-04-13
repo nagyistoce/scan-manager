8c56196ff722b8232097126ae143c6aff596f3f8
oren@andalso.net <oren@andalso.net@be4686e7-f763-aa83-a2ea-18e524bc4576>
2012-04-13 01:54:06 +0000

    Tagging code for build 3. This has working libgphoto2 support for windows as well as other improvements (tabbed camera controls, logging and debugging support, enhanced image viewer, etc.).

---
+++ tags/build3/scanmanager.py
@@ -0,0 +1,79 @@
+from gui.main import App
+import sys
+import traceback
+
+from gui.dialogs import CrashDialog
+
+import log
+import optparse
+import base
+import backend
+
+import resources
+
+
+COMMAND_LINE_HELP = """Usage: scanmanager [--debug]
+	-d, --debug			Run in debug mode (logs detailed information to the console as well as the log file) 
+	-t, --trace		 Log detailed calls for debugging backend APIs 
+"""
+
+def excepthook(excType, excValue, tracebackobj):
+	"""
+	Global catchall for unhandled exceptions
+	"""
+	global app
+	excInfo = (excType, excValue, tracebackobj)
+	text = log.logException(excInfo=excInfo)
+	try:
+		dialog = CrashDialog(parent=app.activeWindow(),html='<pre>%s</pre>'%text)
+		dialog.open()
+	except:
+		pass
+
+
+
+if __name__ == '__main__':
+
+	class MyOptParser(optparse.OptionParser):
+		def print_help(self):
+			print 'ScanManager'
+			print COMMAND_LINE_HELP
+	parser = MyOptParser()
+	parser.add_option('-d','--debug',action='store_true',dest='debug')
+	parser.add_option('-t','--trace',action='store_true',dest='trace')
+	(options, args) = parser.parse_args()
+	
+	base.runtimeOptions.debug = options.debug
+	base.runtimeOptions.trace = options.trace
+
+	if getattr(sys,'frozen',None) == 'windows_exe':
+		class Blackhole(object):
+			softspace = 0
+			def write(self, text):
+				pass
+			def flush(self):
+				pass
+		sys.stdout = Blackhole()
+		sys.stderr = Blackhole()
+
+	try:
+		if base.runtimeOptions.debug:
+			log.configureLogging(fileLevel=log.DEBUG,screenLevel=log.DEBUG)
+		else:
+			log.configureLogging(fileLevel=log.DEBUG,screenLevel=log.WARNING)
+			
+		log.debug('starting up')
+		
+		app = App(sys.argv)
+		sys.excepthook = excepthook
+		app.exec_()
+		
+	finally:
+		
+		for api in backend.apis:
+			try: api.saveSettings()
+			except: pass
+			try: api.close()
+			except: pass
+		try: app.db.close()
+		except: pass
---
+++ tags/build3/setup.py
@@ -0,0 +1,43 @@
+import platform
+from glob import glob
+   
+if platform.system().lower() == 'windows':
+	from distutils.core import setup
+	import py2exe
+	
+	setup(
+		name = "scanmanager",
+		version = "0.2",
+		description = "Tethered shooting GUI for book scanners",
+		data_files = [
+			# Canon PS-ReC dlls
+			('backend/canonpsrec',['backend/canonpsrec/PRLIB.dll','backend/canonpsrec/PRSDK.dll']),
+			# Nikon DLLs and plugins
+			('backend/nikonsdk',['backend/nikonsdk/NkdPTP.dll','backend/nikonsdk/NkdPTPDi.dll','backend/nikonsdk/D80_Mod.md3']),
+            # libgphoto2 backend dlls etc.
+            ('backend/libgphoto2',glob('backend/libgphoto2/*.py')),
+            ('backend/libgphoto2/remote',glob('backend/libgphoto2/remote/*.py')),
+            ('backend/libgphoto2/remote/bin',glob('backend/libgphoto2/remote/bin/*')),
+            ('backend/libgphoto2/win32',glob('backend/libgphoto2/win32/*.dll')),
+            ('backend/libgphoto2/win32',glob('backend/libgphoto2/win32/*.exe')),
+            ('backend/libgphoto2/win32',glob('backend/libgphoto2/win32/*.bat')),
+            ('backend/libgphoto2/win32',glob('backend/libgphoto2/win32/*.txt')),
+            ('backend/libgphoto2/win32/camlibs',glob('backend/libgphoto2/win32/camlibs/*.dll')),
+            ('backend/libgphoto2/win32/iolibs',glob('backend/libgphoto2/win32/iolibs/*.dll')),
+			# Image format plugins for PyQt4
+			('imageformats',glob('d:\Python27\Lib\site-packages\PySide\plugins\imageformats\*')),
+			#glob('d:\Python27\Lib\site-packages\PySide\qt.conf'),
+		],
+		options = dict(
+			py2exe = dict(
+						typelibs = [('{94A0E92D-43C0-494E-AC29-FD45948A5221}', 0, 1, 0)],
+						includes = ['PySide.QtNetwork','dumbdbm','dbhash'],
+			),
+		),
+		#console = ('scanmanager.py',),
+		windows = [
+	       dict(
+				script = 'scanmanager.py',
+			)
+		],	
+	)
